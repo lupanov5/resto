638c7be28c9ae28238785250e0d145bd31415770
Dmitry <lupanov5@mail.ru>
2021-09-06 16:52:00 +0300

    Реализовал загрузку меню из фейкового сервера

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
-import WithRestoService from '../hoc/with-resto-service';
+import { Switch , Route } from 'react-router-dom';
 
 import Background from './food-bg.jpg';
 
-const App = ({RestoService}) => {
-    console.log(RestoService.getMenuItems());
+const App = () => {
     return (
+        
         <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
             <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
+            <Switch>
+                <Route path='/' exact component={MainPage}/>
+                <Route path='/cart' component={CartPage}/>
+                <Route  exact component={MainPage}/>
+            </Switch>          
         </div>
+        
     )
 }
 
-export default WithRestoService()(App);
+export default App;
