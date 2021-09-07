2cc21f749977e4356d62ad328b4386ba9d703936
Dmitry <lupanov5@mail.ru>
2021-09-07 12:17:11 +0300

    Реализовал корзину товаров

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {MainPage, CartPage} from '../pages';
+import {MainPage, CartPage, ItemPage} from '../pages';
 import AppHeader from '../app-header';
 import { Switch , Route } from 'react-router-dom';
 
@@ -11,9 +11,9 @@ const App = () => {
         <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
             <AppHeader total={50}/>
             <Switch>
-                <Route path='/' exact component={MainPage}/>
-                <Route path='/cart' component={CartPage}/>
-                <Route  exact component={MainPage}/>
+                <Route path = '/' exact component={MainPage}/>
+                <Route path = '/cart' exact component={CartPage}/>
+                <Route path = '/:id'  component={ItemPage}/>
             </Switch>          
         </div>
         
